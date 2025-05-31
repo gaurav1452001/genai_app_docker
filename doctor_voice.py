f747997bd48c3b6edf32d0c17383b956ae4db2f0
gaurav <gaurav1452001@gmail.com>
2025-06-01 02:35:10 +0530

    elevenlabs checkpoint 3

--- doctor_voice.py
+++ doctor_voice.py
@@ -16,23 +16,23 @@ text_to_speech_gtts(input_text, "response.mp3")
 
 
 #setup1.b text to speech tts-model for response elevenlabs
-from elevenlabs import generate, save, set_api_key
+from elevenlabs.client import ElevenLabs
+from elevenlabs import save
 
-ELEVEN_LABS_API_KEY = os.getenv("ELEVEN_LABS_API_KEY")
-set_api_key(ELEVEN_LABS_API_KEY)
+def text_to_speech_elevenlabs(input_text, output_path):
+    elevenlabs = ElevenLabs(
+        api_key=os.getenv("ELEVEN_LABS_API_KEY"),
+    )
+    audio = elevenlabs.text_to_speech.convert(
+        text=input_text,
+        voice_id="EXAVITQu4vr4xnSDxMaL",
+        model_id="eleven_turbo_v2",
+        output_format="mp3_44100_128",
+    )
+    save(audio, output_path)
 
-def text_to_speech_elevenlabs(input_text, output_filepath):
-        audio = generate(
-            text=input_text,
-            voice="Sarah",
-            model="eleven_turbo_v2",
-            output_format="mp3_44100_128"
-        )
-        save(audio, output_filepath)
-        print(f"Audio saved successfully to {output_filepath}")
-        
-
-input_text = "Hello, my name is Sarah and i am an awesome girl!"
-text_to_speech_elevenlabs(input_text, output_filepath="response_elevenlabs.mp3")
+# Example usage
+input_text = "Hello! I am a follower of Gaurav Kumar."
+text_to_speech_elevenlabs(input_text, "response_elevenlabs2.mp3")
 
 #use model for text output to voice
