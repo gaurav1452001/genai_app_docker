f5736d253c6bc8ed436736c375b40920ef8e4c62
gaurav <gaurav1452001@gmail.com>
2025-06-01 01:32:50 +0530

    elevenlabs checkpoint 1

--- doctor_voice.py
+++ doctor_voice.py
@@ -11,7 +11,7 @@ def text_to_speech_gtts(input_text, output_path):
         slow=False
     )
     audioobj.save(output_path)
-input_text = "Hello, how can I assist you today?"
+input_text = "Hello, my name is Sarah and i am an awesome girl!"
 text_to_speech_gtts(input_text, "response.mp3")
 
 
@@ -19,16 +19,17 @@ text_to_speech_gtts(input_text, "response.mp3")
 import elevenlabs
 from elevenlabs.client import ElevenLabs
 
-def text_to_speech_gtts(input_text, output_path):
-    language = 'en'
+ELEVEN_LABS_API_KEY = os.getenv("ELEVEN_LABS_API_KEY")
 
-    audioobj=gTTS(
+def text_to_speech_elevenlabs(input_text,output_path):
+    client = ElevenLabs(api_key=ELEVEN_LABS_API_KEY)
+    audio=client.text_to_speech.convert(
         text=input_text,
-        lang=language,
-        slow=False
+        voice_id="EXAVITQu4vr4xnSDxMaL",
+        output_format="mp3_44100_128",
+        model_id="eleven_multilingual_v2",
     )
-    audioobj.save(output_path)
-input_text = "Hello, how can I assist you today?"
-text_to_speech_gtts(input_text, "response.mp3")
+    elevenlabs.save(audio, output_path)
+text_to_speech_elevenlabs("This is a new girl next door",output_path="response_elevenlabs.mp3")
 
 #use model for text output to voice
